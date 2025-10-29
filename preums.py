d7a0a370149718fb34f2ce4c800394a87c0e80e8
esdm1data-2526 <developpeur@esp-esd.com>
2025-10-29 16:44:51 +0100

    Add files via upload

---
+++ preums.py
@@ -0,0 +1,35 @@
+import math
+
+
+animal = "cat"
+age = 3
+weight = 5.6
+nice = False
+
+toys = ["balle", "carton", "mug"]
+
+
+# dictionary
+catStuff = {
+    "toy":"balle",
+    "appliance": "bowl"
+}
+
+def make_pasta(sauce):
+    print("here is some pasta with :")
+    print(sauce)
+
+
+
+
+print("coucou")
+print(toys)
+
+make_pasta("carbonara")
+make_pasta("bolognese")
+
+nombre = input("combien ?")
+
+
+
+print(math.sqrt(int(nombre)))
---
+++ semantruc.py
@@ -0,0 +1,15 @@
+import spacy
+
+model = spacy.load('en_core_web_lg')
+
+
+
+#definir un mto secret
+
+#faire un input pour demander le mot tenté
+
+#utiliser spacy pour déterminer la proximité sémantique
+
+#convertir cette proximité sur une base de 0 à 100°c
+
+#selon la réponse, lui dire s'il est chaud , froid ou s'il a gagné
